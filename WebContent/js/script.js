4c70b5f35ea9b97669d70136cea26921dbbdb3f6
prasanth2049 <v.prasanth.20498@gmail.com>
2019-12-07 15:22:52 +0530

    web module

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,1 +1,32 @@
-// Include form validation functions here
+    function validation() {
+    var a = document.forms["form"]["title"].value;                            
+	if(a=="") {
+		alert("Title is Required.");
+		return false;
+	}
+	  
+	if ((a.length < 2) || (a.length > 30)) {
+        alert("Title should have 2 to 65 characters.");
+        return false;
+    }
+	  
+    var b = document.forms["form"]["Gross($)"].value;
+    if(b=="") {
+        alert("Gross is Required.");
+        return false;
+    }
+    
+    var c = document.forms["form"]["dateOfLaunch"].value;
+    if(c=="") {
+        alert("Date of Launch is required.");
+        return false;
+    }
+    var d = document.forms["form"]["Category"].value;
+    if(d=="") {
+        alert("Category is required.");
+        return false;
+    }
+    }
+	 
+	 
+	 
